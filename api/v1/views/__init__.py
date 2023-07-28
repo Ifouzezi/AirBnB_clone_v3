8f1452ec9222a0a2028983f818a99a8dbba36227
ifouzezi <ifoghaleuzezi@gmail.com>
2023-07-28 15:05:29 +0100

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,15 +2,13 @@
 """Initialize Blueprint views"""
 from flask import Blueprint
 
-# Create a Blueprint named "app_views" with the URL prefix "/api/v1"
-api_v1_blueprint = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# Import view modules from different endpoints
-from api.v1.views.index import index_view
-from api.v1.views.amenities import amenities_view
-from api.v1.views.cities import cities_view
-from api.v1.views.places import places_view
-from api.v1.views.places_amenities import places_amenities_view
-from api.v1.views.places_reviews import places_reviews_view
-from api.v1.views.states import states_view
-from api.v1.views.users import users_view
+from api.v1.views.index import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.places import *
+from api.v1.views.places_amenities import *
+from api.v1.views.places_reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
